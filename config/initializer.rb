05ddcb51e851610cd4a1baa48db525419f011f4e
Petr Balaban <balaban.petr@gmail.com>
2015-05-06 03:14:56 +0300

    Update access_token

--- config/initializer.rb
+++ config/initializer.rb
@@ -1,4 +1,4 @@
 Octokit.configure do |config|
   config.auto_paginate = true
-  config.access_token  = '80c738fd604f33e2f7fb2e3330bde7d38397fc2e'
+  config.access_token  = 'bc82e79f7bc86e5ca6ec6dfe923c5c087fda6378'
 end
